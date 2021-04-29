281bab3fcbcf86e65e742c6542af0547a2d30d4d
wanemar224 <alpha-marouana.diallo9@etu.univ-lorraine.fr>
2021-04-30 00:19:07 +0200

    deployement

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./style/styles.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import bootstrap from 'bootstrap';
-import '@fortawesome/fontawesome-free/js/all.js';
-
-console.log("salut");
